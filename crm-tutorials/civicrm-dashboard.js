ba6208e59e485a7bed9cc2cc7469a5235ff195da
Mathieu Lutfy <mathieu@symbiotic.coop>
2020-05-07 18:34:09 +0000

    Update the Dashboard tutorial to have a more welcome feel.

--- crm-tutorials/civicrm-dashboard.js
+++ crm-tutorials/civicrm-dashboard.js
@@ -1,33 +1,63 @@
 {
     "id": "civicrm-dashboard",
+    "title": ts("Welcome to CiviCRM!"),
     "url": "civicrm/dashboard",
+    "auto_run": true,
     "steps": [
         {
             "target": "#crm-dashboard-configure",
-            "title": ts("Configure your Dashboard"),
+            "title": ts("Welcome to CiviCRM!"),
             "placement": "bottom",
-            "content": ts("Click here to start making changes to the reports that appear on your dashboard."),
+            "content": ts("This is your default CiviCRM dashboard. It includes information to help you get started. You can customize it, but we will get back to this later!"),
             "icon": ""
         },
         {
-            "target": "#3-0",
-            "title": ts("Choose your Reports"),
+            "target": "#crm-qsearch",
+            "title": ts("Search for a Contact"),
             "placement": "bottom",
-            "content": ts("This where you can see all the available reports. Drag the one(s) you want <strong>down</strong> into the right or left column."),
+            "content": ts("You can quickly find a contact by typing in this quicksearch field. By default it will search by Last Name or Email."),
             "icon": ""
         },
         {
-            "target": "#2-0",
-            "title": ts("Removing Reports"),
+            "target": "#civicrm-menu > li[data-name='Search']",
+            "title": ts("Search Tools"),
             "placement": "bottom",
-            "content": ts("If you want to remove a report, you can drag it <strong>up</strong> to the dark grey area above."),
+            "content": ts("You can also explore the other search tools. If you would like to see a list of all contacts, go to Search > Find Contacts, then click the Search button."),
             "icon": ""
         },
         {
-            "target": "#configure-dashlet  .button",
-            "title": "Finish",
+            "target": "#civicrm-menu > li[data-name='Contacts']",
+            "title": ts("Create New Contacts"),
             "placement": "bottom",
-            "content": ts("When you have finished arranging the reports, click here to save the changes."),
+            "content": ts("To create new Contacts, click on the Contacts menu, then New Individual or New Organization."),
+            "icon": ""
+        },
+        {
+            "target": "#civicrm-menu > li[data-name='Home']",
+            "title": ts("CiviCRM Logo"),
+            "placement": "bottom",
+            "content": ts("This logo is also a menu option. It includes a search box that lets you quickly access any menu item. The <em>Hide Menu</em> can be useful to see the menu from your content management system, depending on your permissions."),
+            "icon": ""
+        },
+        {
+            "target": ".show-refresh",
+            "title": ts("Refreshing Dashboard Data"),
+            "placement": "left",
+            "content": ts("Back to the dashboard! You may notice that the dashboard will periodically refresh automatically. You can also force a refresh by clicking on this link."),
+            "icon": ""
+        },
+        {
+            "target": "#crm-dashboard-configure",
+            "title": ts("Configure Your Dashboard"),
+            "placement": "bottom",
+            "content": ts("Finally, you can customize the information seen on this screen, by adding, re-ordering or removing <em>dashlets</em>. There may not be many dashlets available by default. These can be added by administrators in the report configurations."),
+            "icon": ""
+        },
+        {
+            "target": "#civicrm-menu > li[data-name='Support']",
+            "title": ts("Learn more!"),
+            "placement": "bottom",
+            "content": ts("There are many resources to help learn CiviCRM. CiviCRM continues to be created and used by a global community of tens of thousands of individuals and organisations whose fundamental objective is to make a meaningful difference in the world. Thank you for using and supporting CiviCRM!"),
             "icon": ""
         }
     ],
